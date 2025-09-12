7e8fbfa72ccebe001facdadafe1d26ac722de833
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-09-12 21:59:10 +0500

    Math -> random() method V2

--- Math.js
+++ Math.js
@@ -14,8 +14,14 @@
 // console.log(Math.floor(2.8));    //always round down
 
 //random numbers 16 digit decimal numbers between 0 and 1 
+// *c-v2
+
+// console.log(Math.random() );
+
+// console.log(Math.ceil(Math.random() * 6))  //numbers between 1-6
+
+console.log(Math.ceil(Math.random() * 2))  //numbers between 1-2
 
 
-console.log(Math.random() );
 
 
