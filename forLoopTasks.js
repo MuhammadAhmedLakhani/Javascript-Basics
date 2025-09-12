9e81ca53d635b180b3081ac1e558d219b7f6c8a1
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-09-12 15:35:37 +0500

    forLoops ->minimum number extractor from an array

--- forLoopTasks.js
+++ forLoopTasks.js
@@ -5,13 +5,27 @@
 
 //task2 :Extracting Maximum value from an array
 
-var numbers = [12,44,61,54,77,89,23];
-var max = 0;
+// var numbers = [12,44,61,54,77,89,23];
+// var max = 0;
+
+// for(var i = 0;i < numbers.length;i++){
+//     if(numbers[i] > max){
+//         max = numbers[i]
+//     }
+// }
+ 
+// console.log(max);
+
+
+//Task2 : Minimum number extractor
+
+var numbers = [12,44,61,54,7,89,10,23];
+var min = numbers[0];
 
 for(var i = 0;i < numbers.length;i++){
-    if(numbers[i] > max){
-        max = numbers[i]
+    if(numbers[i] < min){
+        min = numbers[i]
     }
+
 }
- 
-console.log(max);
+console.log(min)
