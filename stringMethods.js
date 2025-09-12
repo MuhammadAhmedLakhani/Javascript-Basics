7b27a64b159e833c6f123ba1478f585626e76f6b
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-09-12 18:14:10 +0500

    stringMethods -> slice() method V2

--- stringMethods.js
+++ stringMethods.js
@@ -5,11 +5,14 @@
 //second parameter is index to end the copying however the end index os plus one of actial one as it doescopunt -1 from end
 
 
-// *c v-1
+// *c v-2
 
 
 var str = "Hello World";
 
-console.log(str.slice(0,5));  //Extracting "Hello"
+// console.log(str.slice(0,5));      //Extracting "Hello"
 
-console.log(str.slice(6))       //Extracting "World"
+// console.log(str.slice(6))       //Extracting "World"
+
+
+console.log(str.slice(-5));         //negative indexing
