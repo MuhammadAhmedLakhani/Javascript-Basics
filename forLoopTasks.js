708ca6d459eb16af4df0298ea9fbfa525a9a6706
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-09-12 16:20:30 +0500

    forLoops ->Finding and adding Multiple Consective Missing Numbers in an ordered list

--- forLoopTasks.js
+++ forLoopTasks.js
@@ -33,12 +33,34 @@
 //Task4:Finding Missing number in ordered ascending list and apending it as well in same list
 
 
-var list = [11,12,14,15,16,18,19,20];
+// var list = [11,12,14,15,16,18,19,20];
 
-for(var i = 0;i < list.length;i++){
-    if(list[i + 1] - list[i] > 1){
-        list.splice(i+1,0,list[i] + 1)
+// for(var i = 0;i < list.length;i++){
+//     if(list[i + 1] - list[i] > 1){
+//         list.splice(i+1,0,list[i] + 1)
+//     }
+// }
+// console.log(list);
+
+
+
+
+//Task 5:Finding Multiple consective Missing numbers in an ordered ascending list and apending them as well in same list
+
+
+var num = [51,52,55,56,58,60];
+var gap = 0;
+for(var i = 0;i < num.length;i++){
+    if(num[i + 1] - num[i] === 2){
+        num.splice(i + 1,0,num[i] + 1)
+    }  
+    else if(num[i + 1] - num[i] > 2){
+        gap = num[i + 1] - num[i]
+        for(var j = 1;j < gap;j++){
+            num.splice(i + j,0,num[i] + j)
+        }
     }
 }
-console.log(list);
+console.log(num);
+
 
