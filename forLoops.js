01dd0ffdee0c6e61346c273212c988f59db08874
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-09-12 14:25:45 +0500

    for loops

---
+++ forLoops.js
@@ -0,0 +1,11 @@
+//for loops is use for iterations/repetations of part of code
+//loop save time as no need to write repetitive code
+//first code block is run when condition is true then increment/updation happens after code block has been run 
+//when updation happens lopp started again
+//repetations of lopp depend on updation , when no updation then loop ended
+
+//variable initialization ,condition, updation
+for(i = 0; i < 5; i++){
+    console.log(i)     //loop body / code block
+}
+console.log(i);        //loop stops when i = 5 so answer is 5!
