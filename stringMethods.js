5a00caee42cdc1362335a966f3678fe4a40a13fd
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-09-12 18:27:33 +0500

    stringMethods ->indexOf() method

--- stringMethods.js
+++ stringMethods.js
@@ -1,5 +1,5 @@
 
-//slice method
+//------->lice method
 //slice method allows copying of string elements through indexing just like array
 //first parameter is index of start from which to start copying
 //second parameter is index to end the copying however the end index os plus one of actial one as it doescopunt -1 from end
@@ -8,14 +8,37 @@
 // *c v-3
 
 
-var str = "Hello World";
+// var str = "Hello World";
 
-// console.log(str.slice(0,5));      //Extracting "Hello"
+// // console.log(str.slice(0,5));      //Extracting "Hello"
 
-// console.log(str.slice(6))       //Extracting "World"
-
-
-// console.log(str.slice(-5));         //negative indexing not a better way
+// // console.log(str.slice(6))       //Extracting "World"
 
 
-console.log(str.slice(str.length - 5))  //a btter approach than negative indexing
+// // console.log(str.slice(-5));         //negative indexing not a better way
+
+
+// console.log(str.slice(str.length - 5))  //a btter approach than negative indexing
+
+
+
+//---------->indexOf()
+
+//indexOf gives the first index of value(s) given
+
+var string = "SMIT Saylani";
+
+console.log(string.indexOf("Saylani"))
+
+
+
+
+
+
+
+
+
+
+
+
+
