3de2a2b5f342c44f79fe9a9c218663cb5cc533e5
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-09-12 18:09:50 +0500

    stringMethods ->slice() method

---
+++ stringMethods.js
@@ -0,0 +1,15 @@
+
+//slice method
+//slice method allows copying of string elements through indexing just like array
+//first parameter is index of start from which to start copying
+//second parameter is index to end the copying however the end index os plus one of actial one as it doescopunt -1 from end
+
+
+// *c v-1
+
+
+var str = "Hello World";
+
+console.log(str.slice(0,5));  //Extracting "Hello"
+
+console.log(str.slice(6))       //Extracting "World"
