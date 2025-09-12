6a5dceaca4d28951532a764c0cb23767efd093f1
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-09-12 21:18:23 +0500

    Math ->ceil() method

--- Math.js
+++ Math.js
@@ -1,3 +1,9 @@
 //Math is keyword in javascript M is capital
 
-console.log(Math.round(12.534)); // normal roundoff
+//--->round() method
+
+// console.log(Math.round(12.534)); // normal roundoff
+
+//--->ceil() method
+
+console.log(Math.ceil(11.9));    //always round up
