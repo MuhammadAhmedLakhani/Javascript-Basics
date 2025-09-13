dcb4a5d88e2dfdac5dc73526cca1137e4ad668b5
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-09-13 14:21:31 +0500

    functions->syntax

---
+++ functions.js
@@ -0,0 +1,14 @@
+//functions are predefined code for a soecific task
+//function can be used again and again by just calling them without writing same code again and again
+//naming functions have same rules as of variable naming rules
+//code inside function will not run until its called
+
+
+// *c v-1
+
+//keyword name round brackets curly brackets        -->function syntax
+function abc() {
+    console.log(new Date())
+}
+abc();           ///calling function by their name
+
