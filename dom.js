04e3c4d4a53ffcb97ac5e3c7a6fdbfa029d511e1
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-09-13 15:09:10 +0500

    Dom->wrting values through javascript

---
+++ dom.js
@@ -0,0 +1,12 @@
+//there is a global object in javascript called 'document'
+
+
+// console.log(document.getElementById("main").innerText)
+
+document.getElementById("main").innerText = "Muhammad Ahmed"
+
+//function can be called through html as well
+function display(){
+    console.log(document.getElementById("form").value)
+}
+
