e79ecef4a5527195ee70b6a4f4080a647fc40bc5
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-09-13 10:00:48 +0500

    typeConversion->converting from number datatype to string

--- typeConversion.js
+++ typeConversion.js
@@ -1,16 +1,23 @@
+//---string datatype to number conversion
 
 //parseInt() method
 // removes decimal number and convert from string to number datatype
 
-console.log(parseInt("23.34343"))
+// console.log(parseInt("23.34343"))
 
-//parseFloat() method
-//Does not removes decimal number and convert from string to number datatype
+// //parseFloat() method
+// //Does not removes decimal number and convert from string to number datatype
 
-console.log(parseFloat("23.34343"))
+// console.log(parseFloat("23.34343"))
 
-//Number() method
-//doesnot removes decimal number and convert from string to number datatype and it is the recommended choice for string conversion
+// //Number() method
+// //doesnot removes decimal number and convert from string to number datatype and it is the recommended choice for string conversion
 
-console.log(parseFloat("63.58343")) 
+// console.log(parseFloat("63.58343")) 
+
+
+
+//------Number data type to string conversion
+
+console.log(typeof(3.142.toString()))
 
