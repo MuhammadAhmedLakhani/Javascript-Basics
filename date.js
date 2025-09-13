e94bfc0b55f7fac05906a7727573d8b1eb0548df
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-09-13 11:38:02 +0500

    Date Object V1

---
+++ date.js
@@ -0,0 +1,16 @@
+
+//Date is a constructor function whcih gives the date object
+
+
+// *c v-1
+
+var date = new Date();
+var days = ["sun","mon","tues","wed","thur","fri","sat"]
+// console.log(days[date.getDay()]);
+
+// console.log(date.getDate())
+// console.log(date.getFullYear())
+// console.log(date.getHours())
+// console.log(date.getMinutes())
+// console.log(date.getSeconds())
+console.log(date.getMilliseconds())
