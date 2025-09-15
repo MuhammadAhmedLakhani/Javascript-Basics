e7b97039910aa8c4ad829fca0c68ad9998c91ffa
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-09-16 00:54:05 +0500

    switch statements

---
+++ switch.js
@@ -0,0 +1,18 @@
+//switch statement is useful for only one variable
+
+
+var day = prompt("Enter your Day");
+switch (day){
+    case "tues":
+        console.log("Theory");
+        break
+    case "thurs":
+        console.log("Theory");
+        break
+    case "sat":
+        console.log("Practical");
+        break
+    default:
+        console.log("Nothing");
+        break
+}
