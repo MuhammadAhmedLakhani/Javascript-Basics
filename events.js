58f86e767a875cb0b87bc49bcab00adfd971e91f
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-09-17 12:05:21 +0500

    events->getting image file input and displaying them

--- events.js
+++ events.js
@@ -1,4 +1,23 @@
+//onClick event can be applied to all html elements
+//but it should be use only on clickable items
 
-function clickFunction() {
-    alert("clicked")
+
+// function clickFunction() {
+//     alert("clicked")
+// }
+
+
+
+//---getting file values as images only and displaying them
+
+function submit() {
+    var file = document.getElementById("file");
+    console.log(URL.createObjectURL(file.files[0]))  //making file link
+                                                    //URL is keyword in javascript
+    
+    var image = document.getElementById("image");
+    image.src = URL.createObjectURL(file.files[0])
+    image.style.display = "block"
+
 }
+
