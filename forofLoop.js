d43ff9b301af171b8c8c39a736ef432c5701cda2
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-09-17 10:42:56 +0500

    multilineComment

--- forofLoop.js
+++ forofLoop.js
@@ -3,8 +3,23 @@
 //its a optimized version of loop and fast aswell
 //use it when you donot need to have value of i (index) needed
 
-var stu = ["ghous", "ahmed", "ali", "zain"];
+// var stu = ["ghous", "ahmed", "ali", "zain"];
 
-for(var v of stu){
-    console.log(v)    //printitng array element on each iteration
-}
+// for(var v of stu){
+//     console.log(v)    //printitng array element on each iteration
+// }
+
+
+
+
+
+//-multiLine comment in javascript
+///* content to be commit*/
+//ahort key comment single line comment
+
+
+/*dfkdf
+wqeqwe
+qweqwe
+qweqweqec
+gwrgge */       
