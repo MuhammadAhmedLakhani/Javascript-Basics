7ddcc7034f1373325dfe9508e8fc1e3adb1b00e5
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-09-17 10:25:12 +0500

    doWhileLoop->syntax

---
+++ doWhileLoop.js
@@ -0,0 +1,18 @@
+//do while loop is just like 3 other loops except it checks the condition after it executes (in end)
+//this means this loop wll run ayleast one time for sure
+
+
+// var i = 0
+
+// do{
+//     console.log(i)     //printitng values from 0 to 9    
+//     i++
+// }while(i < 10)
+
+
+var i = 0;
+
+do{ 
+    console.log(i)    running still one time even condition is false
+    i++
+}while(i > 2)
