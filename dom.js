ae67a32dfcfcca8a57033c3772c307460d79bb7a
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-10-23 20:49:58 +0500

    DOM:get/set value thorugh TagName metthod

--- dom.js
+++ dom.js
@@ -6,7 +6,25 @@
 // document.getElementById("main").innerText = "Muhammad Ahmed"
 
 //function can be called through html as well
-function display(){
-    console.log(document.getElementById("form").value)
-}
+// function display(){
+//     console.log(document.getElementById("form").value)
+//
+
+
+//get/set value thorugh TagName metthod
+
+//c:v1
+
+
+//through NagName relevent tag across all document is stored in element variable
+
+
+
+var element = document.getElementsByTagName('div')
+
+console.log(element[2])
+
+
+
+
 
