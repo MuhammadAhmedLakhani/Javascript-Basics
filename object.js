f3f14d62bad750606211470973c14865dfa9fffa
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-10-27 11:47:52 +0500

    Objects:basic syntax

---
+++ object.js
@@ -0,0 +1,21 @@
+///object is special data type used for storing multiple information of same subject/person
+//its a combination of key value pair
+//value can of any data type including array or even object as well(nested object)
+
+
+
+
+
+
+var student = {
+    name: "Ahmed",
+    email:"xyz@gmail.com",
+    number:123141342,
+    active:true,
+    classDays:["tues","thurs","fri"],
+    schoo   l:{
+        name: "SMIT"
+    }
+}
+
+console.log(student)
