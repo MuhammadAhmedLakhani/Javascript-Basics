d80376c08e3290e19cb9a7460cf9168c84b67893
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-11-11 11:49:49 +0500

    Browser:location object

---
+++ browser.js
@@ -0,0 +1,23 @@
+
+
+//Inside 'window' global object there is alot of data related to
+//browser
+//like there is an object called 'location' which has values and methods
+
+//'href' in location refers to url of page (in our case filepath)
+
+console.log(window.location.href)
+
+//getting the file path in our case as href value
+
+// window.location.href =  "https://facebook.com"
+
+
+//replacing the href value to facebook url
+
+
+// location = "https://instagram.com"
+
+//short way to update location href
+//note:you can acess location object directly as well
+
