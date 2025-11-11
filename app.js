5ead474c31f314fd10562180f6b1b184e070e09e
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-11-11 16:23:18 +0500

    Window:open() method

---
+++ app.js
@@ -0,0 +1,15 @@
+//private window forms which is made and can be controlled by javascript
+//open is a method found in global object(window)
+
+//this will open a blank window of maximum size
+
+//this window has its own DOM so can get and set  data
+
+
+var w = window.open()
+
+console.log(w)
+
+//here w will be null if popup blocks from creating new window
+
+
