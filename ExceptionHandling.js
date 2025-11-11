48e1d236a70204dfea5c0f9e62b0225f4535f01b
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-11-11 18:07:28 +0500

    Exceptionhandling:overview

---
+++ ExceptionHandling.js
@@ -0,0 +1,15 @@
+
+//try block trys to run the program inside and looks for errors
+//if error comes as expected the error will be catched in catch block
+
+
+
+
+console.log("Hello")
+console.lof("Hi")
+console.log("Hey")
+
+
+//error in line 9 causing crash of program and no longer runs afterwords
+
+//try catch block solve this issue
