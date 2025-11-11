33fcccc09817b527898fef10fd1c4c1eb59b6a18
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-11-11 18:12:48 +0500

    Excemption Handling:Try-Catch blocks

--- ExceptionHandling.js
+++ ExceptionHandling.js
@@ -5,11 +5,26 @@
 
 
 
-console.log("Hello")
-console.lof("Hi")
-console.log("Hey")
+// console.log("Hello")
+// console.lof("Hi")
+// console.log("Hey")
 
 
 //error in line 9 causing crash of program and no longer runs afterwords
 
-//try catch block solve this issue
+//try catch block solve this issue\
+
+//-try-catch in action
+
+// try block is incomplete without catch block
+//if try run and receives error error is saved in err inside catch which can be handeled
+
+console.log("Hello")
+
+try{
+    console.lof("Hi")
+
+}catch(err){
+    console.log(err)
+}
+console.log("Hye")
