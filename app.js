fded6842eea2cb3d0a1685c9d85ff2316b9e17a6
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-11-11 16:31:37 +0500

    Window:open(URL)

--- app.js
+++ app.js
@@ -5,11 +5,25 @@
 
 //this window has its own DOM so can get and set  data
 
+//v-1
 
-var w = window.open()
+// var w = window.open()
 
-console.log(w)
+// console.log(w)
 
 //here w will be null if popup blocks from creating new window
+
+//v-2
+
+// you can also open particular url link in new window
+
+var newWindow = window.open()
+
+newWindow.location.assign("https://facebook.com")
+
+//opening facebook on new window
+
+
+
 
 
